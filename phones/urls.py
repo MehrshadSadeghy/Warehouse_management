c53afcf10154c832995188c5db64aa0f2ad1e137
Mehrshad <mehrshad83sadeghi@gmail.com>
2024-12-17 15:26:00 +0330

    search by id added

--- phones/urls.py
+++ phones/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import addPhones, redirectToSearchBrandPhone, PhoneByBrandReportView, SameBrandCountryAndPhoneCountryReportView
+from .views import addPhones, redirectToSearchBrandPhone, PhoneByBrandReportView, SameBrandCountryAndPhoneCountryReportView, SearchPhoneModelMixin
 
 urlpatterns = [
     path("ui/v1/phones/new/", addPhones, name="addPhones"),
     path("ui/v1/search_by_brand/", redirectToSearchBrandPhone, name="searchBrand"),
     path("api/v1/brands/<str:brand>/phones/", PhoneByBrandReportView.as_view(), name="searchBrand"),
     path("api/v1/reports/same_brand_country_and_country_search/", SameBrandCountryAndPhoneCountryReportView.as_view(), name="same country"),
+    path("api/v1/phone/<str:pk>/", SearchPhoneModelMixin.as_view, name="phone"),
 ]
